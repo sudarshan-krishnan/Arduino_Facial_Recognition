c765fa8583fe288b1c4b629f312bba0ca7af43b2
Prakhar Verma <unstoppableprakhar@gmail.com>
2024-04-04 23:42:53 -0400

    removed extra

--- main.py
+++ main.py
@@ -1,41 +1,25 @@
-
-
 import cv2
 from cvzone.FaceDetectionModule import FaceDetector
 from cvzone.SerialModule import SerialObject
 
-# Initialize the video capture and the detector
 cap = cv2.VideoCapture(0)
 detector = FaceDetector()
 arduino = SerialObject('/dev/cu.usbmodem11101', 9600)  
 
 while True:
-    # Read a new frame from the video capture
     success, frame = cap.read()
-
-    # Check if the frame was captured successfully
     if success:
-        # Detect faces in the frame
         img, bboxes = detector.findFaces(frame)
-
-        # Check if any faces are detected
         if bboxes:
-            # Send data to Arduino when a face is detected
             arduino.sendData([1, 0])
         else:
-            # Send different data to Arduino when no faces are detected
             arduino.sendData([0, 1])
-
-        # Display the frame with detected faces
         cv2.imshow("Video", img)
-
-        # Check for the 'q' key to break out of the loop
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         print("Failed to capture frame")
         break
 
-# Release the video capture and close all windows
 cap.release()
 cv2.destroyAllWindows()
